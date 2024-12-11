440103cd7b4e57e240c1084f05691ca6a1aca7c2
highsiverr <ye90202@gmail.com>
2024-12-11 14:49:57 +0900

    feat: NHN클라우드 Log & Crash Search 설정

--- src/main/java/com/nhnacademy/hello/HelloApplication.java
+++ src/main/java/com/nhnacademy/hello/HelloApplication.java
@@ -2,12 +2,17 @@ package com.nhnacademy.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class HelloApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
+        logger.error("에러테스트");
     }
 
 }
