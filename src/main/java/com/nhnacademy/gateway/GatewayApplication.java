f032c3dd2f80be26570ba61714fe6d0464e9c9ff
highsiverr <ye90202@gmail.com>
2025-01-02 22:39:21 +0900

    fix: 에러테스트 삭제

--- src/main/java/com/nhnacademy/gateway/GatewayApplication.java
+++ src/main/java/com/nhnacademy/gateway/GatewayApplication.java
@@ -8,11 +8,8 @@ import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class GatewayApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-        logger.error("에러테스트");
     }
 
 }
